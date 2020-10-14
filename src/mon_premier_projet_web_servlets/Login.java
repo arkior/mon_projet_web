3f026e8e5e6815d72e2dc68e8d42f72c39b72606
Christophe Gautherot <gchristophe96@gmail.com>
2020-10-14 17:11:05 +0200

    mon projet web version 1

---
+++ src/mon_premier_projet_web_servlets/Login.java
@@ -0,0 +1,65 @@
+package mon_premier_projet_web_servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		//ouverture d'un flux donc try
+		try (ServletOutputStream out = response.getOutputStream())
+		{
+			out.println("<HTML>\n");
+			out.println("<HEAD>\n");
+			out.println("<TITLE>Hello</TITLE>\n");
+			out.println("</HEAD>\n");
+			out.println("<BODY>\n");
+			out.println("<H1>couou</H1>\n");
+			out.println("</BODY>\n");
+			out.println("</HTML>");
+		}
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doHead(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doHead(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
